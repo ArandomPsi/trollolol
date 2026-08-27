eb4838ed5fad4df3f023fe9deca3e427b8cc35aa
Kosm0-o <logantreboutat@gmail.com>
2026-08-26 20:51:14 -0700

    troll randomness

--- scenesandscripts/jumpscare.gd
+++ scenesandscripts/jumpscare.gd
@@ -2,7 +2,8 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if randi_range(0, 100) >= 25:
+	if randi_range(0, 100) <= 25:
+		print("no jumpscare")
 		queue_free()
 	await get_tree().create_timer(randf_range(0.5, 5.0)).timeout
 	create_jumpscare()
--- scenesandscripts/troll_1.gd
+++ scenesandscripts/troll_1.gd
@@ -3,7 +3,18 @@ extends Area2D
 @export var speed : float = 0.8
 @export var target_y : float = -1200.0
 @export var freeable : bool = true
+@export var random : bool = false
+@export var tethered_troll : Node2D # if this one doesn't go, the other will activate
+var active : bool = false
 
+func _ready() -> void:
+	if random:
+		active = randi_range(0, 1) == 1
+		if not active and random:
+			if tethered_troll != null:
+				tethered_troll.get_node("CollisionShape2D").disabled = false
+		elif tethered_troll != null:
+			tethered_troll.get_node("CollisionShape2D").disabled = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -11,6 +22,10 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
+	print(name, " READY | random=", random, " active=", active)
+	if random:
+		if not active:
+			return
 	var tween = create_tween()
 	tween.tween_property($spike,"position:y",target_y,speed)
 	await tween.finished
