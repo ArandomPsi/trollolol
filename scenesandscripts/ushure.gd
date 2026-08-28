6450449c72a6236137b0384677e14d433432c16a
ArandomPsi <116410822+ArandomPsi@users.noreply.github.com>
2026-08-27 18:22:56 -0700

    floating spike lol

--- scenesandscripts/ushure.gd
+++ scenesandscripts/ushure.gd
@@ -5,5 +5,5 @@ func usuremessage():
 	scale = Vector2(0,0)
 	visible = true
 	var tween = create_tween()
-	tween.tween_property(self,"scale",Vector2(1,1),1.2).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT_IN)
+	tween.tween_property(self,"scale",Vector2(1,1),1.2).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
 	tween.parallel().tween_property(self,"rotation",TAU,1).set_trans(Tween.TRANS_ELASTIC)
