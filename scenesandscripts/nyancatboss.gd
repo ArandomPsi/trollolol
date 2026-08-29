b02e4d4301c7a0fa9152187d964b6dba1eed2045
Kosm0-o <logantreboutat@gmail.com>
2026-08-28 21:52:58 -0700

    nyancatboss start

--- scenesandscripts/nyancatboss.gd
+++ scenesandscripts/nyancatboss.gd
@@ -1,28 +1,46 @@
 extends Node2D
 
-var moving : bool = false
-
-func _ready() -> void:
-	await get_tree().create_timer(0.5).timeout
-	tween()
+var tracking : bool = true
+var moving : bool = true
+var atktimer : float = 0.0
 
 func _process(delta: float) -> void:
+	global_position.x = global.playerpos.x - 576.0
+	if tracking: 
+		global_position.y = global.playerpos.y
+		atktimer += delta
 	if moving:
 		for rainbow in $trail.get_children():
 			rainbow.position.x += 7
 			if rainbow.position.x >= 125.0:
 				rainbow.position.x = -1210.0
+	if atktimer >= 5.0:
+		readyup()
+		atktimer = 0.0
 
 func tween():
 	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(self, "position:x", 1306.0, 0.6)
+	tween.tween_property(self, "global_position:x", global_position.x + 1306.0, 0.6)
 	moving = false
 	for i in range($trail.get_children().size()):
 		tween.parallel().tween_property($trail.get_child($trail.get_children().size() - i - 1), "modulate:a", 0.0, 0.8).set_delay(i * 0.05)
-	tween.tween_property(self, "position:x", 154.0, 0.8)
+	tween.tween_property(self, "global_position:x", global_position.x - 1306.0, 0.8)
 	await tween.finished
 	moving = true
 	for rainbow in $trail.get_children():
 		rainbow.modulate.a = 1.0
-	tween()
 		
+func readyup():
+	tracking = false
+	$path.show()
+	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	for i in range(3):
+		tween.tween_property($path, "modulate:a", 1.0, 0.5)
+		tween.tween_property($path, "modulate:a", 0.0, 0.5)
+	await tween.finished
+	$path.hide()
+	await tween()
+	tracking = true
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	body.die()
--- scenesandscripts/nyancatstart.gd
+++ scenesandscripts/nyancatstart.gd
@@ -4,3 +4,8 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	$"../ParallaxBackground/ParallaxLayer".hide()
 	$"../ParallaxBackground/ParallaxLayer2".show()
+	spawnnyancat()
+
+func spawnnyancat():
+	var nyancatboss = preload("res://scenesandscripts/nyancatboss.tscn").instantiate()
+	get_tree().current_scene.add_child(nyancatboss)
